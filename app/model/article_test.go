433e2bb0e12d5ff7aaf93946abf97685e3101fa0
harventhy <syukri.mullia@gmail.com>
2019-05-05 17:15:44 +0700

    red test_getter

--- app/model/article_test.go
+++ app/model/article_test.go
@@ -13,4 +13,15 @@ func TestArticleModel(t *testing.T) {
 	t.Run("test_new_article", func(t *testing.T) {
 		assert.NotNil(t, article, "article should not be nil")
 	})
+
+	t.Run("test_getter", func(t *testing.T) {
+		assert.Empty(t, article.Id(), "id should be empty")
+		assert.Empty(t, article.Title(), "title should be empty")
+		assert.Empty(t, article.Content(), "content should be empty")
+		assert.Empty(t, article.Author(), "author should be empty")
+		assert.Empty(t, article.CreatedAt(), "createdAt should be empty")
+		assert.Empty(t, article.CreatedBy(), "createdBy should be empty")
+		assert.Empty(t, article.UpdatedAt(), "updatedAt should be empty")
+		assert.Empty(t, article.UpdatedBy(), "updatedBy should be empty")
+	})
 }
