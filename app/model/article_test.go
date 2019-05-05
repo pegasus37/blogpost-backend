175ed91b08f3cd40286ad3364835345669dda49e
harventhy <syukri.mullia@gmail.com>
2019-05-05 17:21:27 +0700

    red test_setter

--- app/model/article_test.go
+++ app/model/article_test.go
@@ -24,4 +24,18 @@ func TestArticleModel(t *testing.T) {
 		assert.Empty(t, article.UpdatedAt(), "updatedAt should be empty")
 		assert.Empty(t, article.UpdatedBy(), "updatedBy should be empty")
 	})
+
+	t.Run("test_setter", func(t *testing.T) {
+		article.SetTitle("a title")
+		article.SetContent("some contents")
+		article.SetAuthor("an author")
+		article.SetCreatedBy("a creator")
+		article.SetUpdatedBy("an updater")
+
+		assert.Equal(t, "a title", article.Title(), "title should be set")
+		assert.Equal(t, "some contents", article.Content(), "content should be set")
+		assert.Equal(t, "an author", article.Author(), "author should be set")
+		assert.Equal(t, "a creator", article.CreatedBy(), "createdBy should be set")
+		assert.Equal(t, "an updater", article.UpdatedBy(), "updatedBy should be set")
+	})
 }
