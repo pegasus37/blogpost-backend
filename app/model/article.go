e0b860124de4d94fa4b10b79bd0e44aae10f6204
harventhy <syukri.mullia@gmail.com>
2019-05-05 17:17:09 +0700

    green test_getter

--- app/model/article.go
+++ app/model/article.go
@@ -12,6 +12,38 @@ type Article struct {
 	updatedBy string
 }
 
+func (a Article) Id() int64 {
+	return a.id
+}
+
+func (a Article) Title() string {
+	return a.title
+}
+
+func (a Article) Content() string {
+	return a.content
+}
+
+func (a Article) Author() string {
+	return a.author
+}
+
+func (a Article) CreatedAt() int64 {
+	return a.createdAt
+}
+
+func (a Article) CreatedBy() string {
+	return a.createdBy
+}
+
+func (a Article) UpdatedAt() int64 {
+	return a.updatedAt
+}
+
+func (a Article) UpdatedBy() string {
+	return a.updatedBy
+}
+
 func NewArticle() *Article {
 	return &Article{}
 }
